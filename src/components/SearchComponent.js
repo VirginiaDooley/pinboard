4eeb9d37d5c1ac3bc9d62dc00052b3962111c2b1
Virginia Dooley <vcdooley@gmail.com>
2019-07-06 17:00:40 +0100

    initial state, search and submit event handlers

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-class SearchComponent extends React.Component {
+class SearchComponent extends Component {
+
+    constructor(){
+      super()
+        this.state = {
+        images: [],
+        query: ""
+      }
+    }
+
+  handleChange = (event) => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // pinterest fetch api will go here
+  }
+
   render () {
     return (
-      <form>
-        <input className="search-form" type="text" placeholder="Search images" />
-        <button type="submit">Submit</button>
-      </form>
+      <div className="search-form">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={event => this.handleChange(event)} value={this.state.value} placeholder="Search images" />
+          <div>
+            <button type="submit">Submit</button>
+          <div>
+        </form>
+      </div>
     )
   }
 }
+
+
 
 export default SearchComponent;
