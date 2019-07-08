c35f5d6ace0c5208cb3f3f2aaade456f0ad247c8
Virginia Dooley <vcdooley@gmail.com>
2019-07-08 17:56:15 +0100

    render images

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const SearchResults = (props, index) =>
-      <div className="image-results">
-        <ol>
-          {props.images.map((image, index) => (
+  <div className="image-results">
+    // <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <ul>
+        {props.images.map((image, index) => (
           <div className="image" key={index}>
-            <li>
-              <p>Url: {image.urls.thumb}</p>
-            </li>
+            <img src={image.urls.thumb} alt={image.alt_description}/>
           </div>
-          ))}
-        </ol>
-      </div>
+        ))}
+      </ul>
+    // </div>
+  </div>
 
 export default SearchResults;
