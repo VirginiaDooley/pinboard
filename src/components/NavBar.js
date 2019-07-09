7d1bb964a64ce116b41a7805f04eb4cfb4f74d1d
Virginia Dooley <vcdooley@gmail.com>
2019-07-09 12:33:12 +0100

    navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,27 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import SearchContainer from '../containers/SearchContainer'
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'grey',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
-    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+    <div style={{ borderBottom: '1px solid grey', paddingBottom: '5px', paddingTop: '5px', marginBottom: '12px', marginTop: '12px' }}>
 
       <NavLink
-        style={{ marginRight: '10px' }}
+        style={link}
         to="/"
       >
         Home
       </NavLink>
 
-      <NavLink
-        style={{ marginRight: '10px' }}
+      <NavLink style={link}
         to="/new"
       >
         Create New Board
