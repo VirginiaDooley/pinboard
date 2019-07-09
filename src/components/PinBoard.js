544e0d04337bd2b4902d4ce1413ac608aa8e952f
Virginia Dooley <vcdooley@gmail.com>
2019-07-09 10:28:40 +0100

    container and component for pinboard

---
+++ src/components/PinBoard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PinBoard = (props) => {
+  return (
+    <div />
+  )
+}
+
+export default PinBoard
---
+++ src/containers/PinBoardContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PinBoard from '../components/PinBoard'
+
+class PinBoardContainer extends React.Component {
+  render () {
+
+  }
+}
+
+export default PinBoardContainer;
