c426bb6258f74b4cbb8c2b9d51879eca4d6d91ff
Virginia Dooley <vcdooley@gmail.com>
2019-07-09 12:14:29 +0100

    grid system

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
 const SearchResults = (props, index) =>
   <div className="image-results">
-      <ul>
-        {props.images.map((image, index) => (
-          <div className="image" key={index}>
-            <img src={image.urls.thumb} alt={image.alt_description}/>
-          </div>
-        ))}
-      </ul>
+    {props.images.map((image, index) => (
+      <div className="image" key={index}>
+        <Container>
+          <Row>
+            <Col size="12" md="8">
+              <img src={image.urls.thumb} alt={image.alt_description}/>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    ))}
   </div>
 
 export default SearchResults;
