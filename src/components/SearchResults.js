a79c63fb7a4075b970e68c8d2af99bfde9e2388d
Virginia Dooley <vcdooley@gmail.com>
2019-07-17 17:33:37 +0100

    remove images map and add div tags

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,7 +3,9 @@ import React from 'react'
 const SearchResults = ({image}) =>
   <div className="grid-container">
       <div className="grid-item">
-        <img src={image.urls.thumb} alt={image.alt_description}/>
+        <div className="image">
+          <img src={image.urls.thumb} alt={image.alt_description}/>
+        </div>
       </div>
   </div>
 
