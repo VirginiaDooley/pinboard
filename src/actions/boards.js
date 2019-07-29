d4eda5d106f5dc5d0cbf7ae266fa7659288a30f7
Virginia Dooley <vcdooley@gmail.com>
2019-07-29 13:32:30 -0700

    refactor to fix mismatch in variables and flow

--- src/actions/boards.js
+++ src/actions/boards.js
@@ -3,3 +3,46 @@
 //   type: 'ADD_IMAGE',
 //   newImage: [...this.state.boardImages, event.target.src]
 // })
+
+
+// export const addImage = image => {
+//   return { type: 'ADD_IMAGE', image };
+// };
+//
+// const API_URL = 'http://localhost:3001';
+// export function showBoard() {
+//   return (dispatch) => {
+//     dispatch({ type: 'LOADING_BOARD' });
+//
+//   return fetch(`${API_URL}/boards`)
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(data => dispatch({ type: 'SHOW_BOARD', payload: data.boards}))
+//   .catch(err => console.log(err));
+//   }
+// }
+
+/
+// export const createBoard = (boards, routerHistory) => {
+//   return dispatch => {
+//     return fetch(`${API_URL}/boards`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ board })
+//     })
+//     .then(res => res.json())
+//     .then(board => {
+//       if (board.id) {
+//           dispatch({ type: 'CREATE_BOARD', payload: board.id}));
+//           routerHistory.push(`${board.id}`);
+//       } else {
+//         const err = board.errors.join('\n\n')
+//         alert(`${board.message}\n\n${err}`)
+//       }
+//     }).catch(err => console.log(err));
+//   };
+// };
+//
--- src/reducers/manageBoards.js
+++ src/reducers/manageBoards.js
@@ -1,26 +1,19 @@
-///reducer///
+///reducer; returns a new state///
 
 const initialState = {
   loading: false,
-  boards: [],
   boardImages: []
 }
+
 export default (state = initialState, action) => {
 
   switch (action.type) {
 
-    case 'LOADING_IMAGE_SEARCH': {
-      return {...state, loading: true};
+    case 'ADD_IMAGE_TO_BOARD': {
+      // debugger
+      return { boardImages:
+        [...state.boardImages, action.newBoardImages]};
     }
-
-    case 'FETCH_IMAGES': {
-      return {...state, loading: false, images: action.payload};
-      // return state.concat(action.images);
-    }
-
-    // case 'ADD_IMAGE_TO_BOARD': {
-    //   return state.concat(action.searchResults);
-    // }
 
     // case 'CREATE_BOARD': {
     //
