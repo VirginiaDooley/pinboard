08c7a9b8aaa040a4d5f6c988e56038f1efc9937a
Virginia Dooley <vcdooley@gmail.com>
2019-07-29 13:33:42 -0700

    remove until further notice

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
-
-class SearchResults extends Component {
-
-  renderImages = (props) => this.props.searchResults.map(image =>
-  <img
-     key={image.id}
-     src={image.urls.small}
-     alt={image.description}
-     name={image}
-   />)
-
-  render() {
-
-     return (
-         <Row>
-           <Col>
-             {this.renderImages()};
-           </Col>
-         </Row>
-     )
-   }
-}
-
-export default SearchResults;
+// import React, { Component } from 'react';
+// import { Row, Col } from 'reactstrap';
+//
+// class SearchResults extends Component {
+//
+//   renderImages = (props) => this.props.searchResults.map(image =>
+//   <img
+//      key={image.id}
+//      src={image.urls.small}
+//      alt={image.description}
+//      name={image}
+//      onClick={this.props.chooseImage}
+//    />)
+//
+//   render() {
+//
+//      return (
+//          <Row>
+//            <Col>
+//              {this.renderImages()}
+//            </Col>
+//          </Row>
+//      )
+//    }
+// }
+//
+// export default SearchResults;
