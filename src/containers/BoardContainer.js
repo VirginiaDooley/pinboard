605a8e098fed0aa2b11646a6ceddf31f36946002
Virginia Dooley <vcdooley@gmail.com>
2019-07-31 07:54:04 -0700

    add input for board title and create state title

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -4,41 +4,65 @@ import { connect } from 'react-redux';
 
 class BoardContainer extends Component {
 
+  state = {
+    title: ''
+  }
   renderBoardImages = () => {
     const boardImages = this.props.boardImages;
-      if (boardImages.length > 0) {
-        return (
-          boardImages.map((image, index) => {
-            return <BoardShow 
-              id={image.id}
-              key={index}
-              src={image.src}
-              alt={image.alt}
+    if (boardImages.length > 0) {
+      return (
+        boardImages.map((image, index) => {
+          return <BoardShow
+            id={image.id}
+            key={index}
+            src={image.src}
+            alt={image.alt}
             />
         }))
-        }
-        return (
-          <h5>
-            Click images from your search results
-            to add them to your board.
-          </h5>
+      }
+      return (
+        <h5>Click images from your search results
+          to add them to your board.</h5>
         )
     }
 
-  render() {
-    return (
-      <div>
-        {this.renderBoardImages()}
-      </div>
-    )
-  }
-}
+    handleTitleChange = (event) => {
+      event.preventDefault()
+      this.setState({
+        [event.target.name]: event.target.value
+      })
+    }
+    //
+    // handleSave = (event) => {
+    //   event.preventDefault();
+    //   const params = {
+    //     board: {
+    //       title:,
+    //       image_attributes: {
+    //         images: this.props.boardImages
+    //       }}
+    //     }
 
-const mapStateToProps = state => {
-  console.log(state)
-  return {
-    boardImages: state.manageBoards.boardImages
+    render() {
+      return (
+        <div className="title-form">
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            placeholder="Add your board title form here."
+            onChange={this.handleTitleChange}/>
+          {this.renderBoardImages()}
+        </div>
+      )
+    }
   }
-}
 
-export default connect(mapStateToProps)(BoardContainer);
+  const mapStateToProps = state => {
+    console.log(state)
+    return {
+      boardImages: state.manageBoards.boardImages
+    }
+  }
+
+  export default connect(mapStateToProps)(BoardContainer);
