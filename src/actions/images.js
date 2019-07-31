f0eee96eae26d7431f5a0a90bbfcc7448ec8bf43
Virginia Dooley <vcdooley@gmail.com>
2019-07-31 11:03:03 -0700

    remove comments

--- src/actions/images.js
+++ src/actions/images.js
@@ -1,24 +1,5 @@
 //actions//
-
 export const addImageToBoard = boardId => ({
   type: 'ADD_IMAGE_TO_BOARD',
   newBoardImages: boardId
-
 })
-
-// #index
-// export const boardList = () => ({
-//
-// build fetch request
-// })
-
-// #new
-// export const createBoard = () => ({
-//
-// })
-
-// #show
-// export const showBoard = boardId => ({
-//
-// build fetch request
-// })
