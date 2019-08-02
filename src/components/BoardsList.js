4aa601878c29f77bc6573a58e88e625700c86d18
Virginia Dooley <vcdooley@gmail.com>
2019-08-02 10:26:30 -0700

    map and render all boards in index route

--- src/components/BoardsList.js
+++ src/components/BoardsList.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const BoardsList = (props) => {
+const BoardsList = ({boards}) => {
+  const renderBoards = boards.map((board, index) =>
+  <Link key={index} id={board.id} title={board.title} to={`/boards/${board.id}`}>{board.title}</Link>
+)
   return (
-    <div />
-  )
-}
+    <div>
+    {renderBoards}
+  </div>
+  );
+};
 
-export default BoardsList
+export default BoardsList;
