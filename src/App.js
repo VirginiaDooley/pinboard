45ad21e333a5f7c56e0726c944578c44f409252d
Virginia Dooley <vcdooley@gmail.com>
2019-08-07 14:13:10 -0400

    unnested boards and move fetch boards in componentDidMount from BoardContainer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { connect } from 'react-redux'
-// App is a parent component
 import './App.css';
 import NavBar from './components/NavBar';
-// SearchContainer is a parent to others but a child component of App
 import SearchContainer from './containers/SearchContainer'
+import CreateBoard from './components/CreateBoard'
 import BoardContainer from './containers/BoardContainer'
-import BoardsPage from './components/BoardsPage';
+import BoardsList from './components/BoardsList';
 import BoardShow from './components/BoardShow';
+import { fetchBoards } from './actions/boards';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchBoards()
+  }
 
   render () {
     return (
       <Router>
         <div className="App">
+
           <div className="App-header">
             <NavBar />
           </div>
+
           <div className="column">
             <SearchContainer />
           </div>
 
-          <Route exact path="/show" component={BoardShow} />
-          <Route exact path="/boards" render={boards =>
-              <BoardsPage {...boards} boards={this.props.boards} />} />
+          <Route exact path="/boards"
+            render={routerProps =>
+              <BoardsList
+                boards={this.props.boards} {...routerProps} />}
+                />
+
+            <Route exact path='/boards/:boardId'
+              render={routerProps =>
+                <BoardShow
+                  boards={this.props.boards} {...routerProps}/>}
+                />
 
             <div className="grid-container">
-              <BoardContainer />
+
+                <div className="grid-item">
+                  <div className="column">
+                    <CreateBoard />
+                  </div>
+                </div>
+
+                <div className="grid-item">
+                  <div className="column">
+                    <BoardContainer />
+                  </div>
+                </div>
+
             </div>
-            </div>
-        </Router>
-      );
-    }
+          </div>
+      </Router>
+    );
+  }
 }
 
 const mapStateToProps = state => {
@@ -43,4 +69,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, {fetchBoards})(App);
