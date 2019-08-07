60bf9a31aacfede7c774e84f9e0449ab377e7dd9
Virginia Dooley <vcdooley@gmail.com>
2019-08-07 14:10:52 -0400

    unnest boards and match params to render board

--- src/components/BoardShow.js
+++ src/components/BoardShow.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react'
 
- const BoardShow = (props) => {
-  return (
-    <img id={props.id} src={props.src} alt={props.alt}/>
-  )
+class BoardShow extends Component {
+ render () {
+   const {boards, match} = this.props;
+   const board = boards.find(board => {
+     if (board.id === parseInt(match.params.boardId)) {
+       return board
+      }
+    })
+   console.log(board)
+   return (
+    <div>
+      <h5>Board Title: {board.title}</h5>
+      // <img>{board.src}</img>
+    </div>
+   )
+ }
 }
 
 export default BoardShow;
