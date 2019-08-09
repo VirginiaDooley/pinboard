092cdeb61cf3964e13003fac6a68420d6e490da4
Virginia Dooley <vcdooley@gmail.com>
2019-08-09 11:46:13 -0400

    add description and className

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="grid-item">
-      <h4>Find your inspiration.</h4>
+    <div className="grid-container">
+      <div className="grid-item">
+        <h1>Pinboard</h1>
+        <h4>Visualise your goals with an inspiration board. Search and "pin" a collection of images.</h4>
+      </div>
     </div>
   );
 };
