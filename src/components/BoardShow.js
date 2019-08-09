f8f8935c5cd4423efb0e9606805f296404f131a5
Virginia Dooley <vcdooley@gmail.com>
2019-08-09 11:43:18 -0400

    map and render board show images

--- src/components/BoardShow.js
+++ src/components/BoardShow.js
@@ -2,23 +2,29 @@ import React from 'react'
 
 const BoardShow = ({boards, match}) => {
 
-  // const renderImages = () => props.boards.map(image =>
-  // <img
-  //    key={image.id}
-  //    src={image.url}
-  //    alt={image.description}
-  //  />)
-
   const board = boards.find(board => {
     if (board.id === parseInt(match.params.boardId)) {
       return board
      }
+     // add error message?
    })
   console.log(board)
+
+  const renderImages = () => board.images.map(image =>
+      <img
+       key={image.id}
+       src={image.url}
+       alt={image.description}
+     />
+  )
+
   return (
+
    <div className="grid-item">
      <h3>Board Title: {board.title}</h3>
+     {renderImages()}
    </div>
+
   )
 }
 
