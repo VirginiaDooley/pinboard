55bcbad1dee901f41bab6c256dad6b860d848535
Virginia Dooley <vcdooley@gmail.com>
2019-08-09 07:19:37 -0400

    change to functional component

--- src/components/BoardsList.js
+++ src/components/BoardsList.js
@@ -1,27 +1,52 @@
 import { Link } from 'react-router-dom';
-import React, {Component} from 'react'
+// import React, {Component} from 'react'
 
-class BoardsList extends Component {
-  render () {
-    const {boards} = this.props;
-    const renderBoards = Object.values(boards).map((board, index) => {
-      return (
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BoardsList = ({boards}) => {
+  const renderBoards = Object.values(boards).map((board, index) => {
+    return (
       <li key={index}>
         <Link to={`/boards/${board.id}`}>{board.title}</Link>
       </li>
       )}
-  )
+    )
 
-    return (
-      <div className="grid-item">
-        <h3>Select a board to view images</h3>
-          <ol>
-            {renderBoards}
-          </ol>
-      </div>
-    );
-
-  }
+  return (
+    <div className="grid-item">
+      <h3>Select a board to view images</h3>
+        <ol>
+          {renderBoards}
+        </ol>
+    </div>
+  );
 }
 
-export default BoardsList;
+export default BoardsList
+//
+//
+// class BoardsList extends Component {
+//   render () {
+//     const {boards} = this.props;
+//     const renderBoards = Object.values(boards).map((board, index) => {
+//       return (
+//       <li key={index}>
+//         <Link to={`/boards/${board.id}`}>{board.title}</Link>
+//       </li>
+//       )}
+//   )
+//
+//     return (
+//       <div className="grid-item">
+//         <h3>Select a board to view images</h3>
+//           <ol>
+//             {renderBoards}
+//           </ol>
+//       </div>
+//     );
+//
+//   }
+// }
+//
+// export default BoardsList;
